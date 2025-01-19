8dd5599ade9d68453fddcdbadb15459583061e7b
Arnaldo Junior <abs.junnior@hotmail.com>
2025-01-19 12:27:57 -0300

    Created project

---
+++ src/main/java/com/absjr/apieasyparking/ApiEasyParkingApplication.java
@@ -0,0 +1,13 @@
+package com.absjr.apieasyparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEasyParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEasyParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/absjr/apieasyparking/ApiEasyParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.absjr.apieasyparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEasyParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
